b04851c6e224aa8abcdfb0f9d67e0317d2cbd545
tkburis <tkburis@gmail.com>
2021-06-17 12:35:45 +0100

    Added solutions

--- 4_palindrome_prod.py
+++ 4_palindrome_prod.py
@@ -11,12 +11,58 @@ class Solution:
         _min = self._min
         _max = self._max
         curr_max = 0
-        # start from _max, then go down
-        for i in range(_max, _min-1, -1):
-            for j in range(i, _min-1, -1):
+        c = 0
+
+        for i in range(_min, _max+1):
+            for j in range(_min, _max+1):
                 curr = i * j
                 if self.is_palindrome(curr):
-                    curr_max = max(curr, curr_max)
+                    curr_max = max(curr_max, curr)
+                c += 1
+        print(f"Products checked: {c}")
+        return curr_max
+
+    def count_backwards(self) -> int:
+        _min = self._min
+        _max = self._max
+        curr_max = 0
+        c = 0
+
+        for i in range(_max, _min-1, -1):
+            for j in range(_max, i-1, -1):
+                curr = i * j
+                if curr <= curr_max:  # because we are counting down, from here i*j < curr_max, so just skip to next i
+                    break
+                if self.is_palindrome(curr):  # this condition is reached only if curr > curr_max
+                    curr_max = curr
+                c += 1
+
+        print(f"Products checked: {c}")
+        return curr_max
+
+    def mults_eleven(self) -> int:
+        _min = self._min
+        _max = self._max
+        curr_max = 0
+        c = 0
+
+        for i in range(_max, _min-1, -1):
+            if i % 11 == 0:
+                for j in range(_max, i-1, -1):
+                    curr = i * j
+                    if curr <= curr_max:
+                        break
+                    if self.is_palindrome(curr):
+                        curr_max = curr
+            else:  # at least one of i/j must be divisible by 11. so if i not divisible, j must be mult of 11
+                max_j = _max - (_max % 11)
+                for j in range(max_j, i-1, -11):
+                    curr = i * j
+                    if curr <= curr_max:
+                        break
+                    if self.is_palindrome(curr):
+                        curr_max = curr
+
         return curr_max
 
     @staticmethod
@@ -31,3 +77,7 @@ if __name__ == "__main__":
     sol = Solution(100, 999)  # 3-digits
     with Timer():
         print(sol.naive())
+    with Timer():
+        print(sol.count_backwards())
+    with Timer():
+        print(sol.mults_eleven())
